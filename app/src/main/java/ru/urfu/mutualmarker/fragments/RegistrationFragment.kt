d21b943057d2ab55780c4aa2755dbc0de4edaa2b
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-11-05 10:28:20 +0500

    fix and refactoring

--- app/src/main/java/ru/urfu/mutualmarker/fragments/RegistrationFragment.kt
+++ app/src/main/java/ru/urfu/mutualmarker/fragments/RegistrationFragment.kt
@@ -2,10 +2,13 @@ package ru.urfu.mutualmarker.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
@@ -42,31 +45,86 @@ class RegistrationFragment : Fragment() {
         val view = requireView();
         val emailField = view.findViewById<TextInputEditText>(R.id.EmailField)
         val passwordField = view.findViewById<TextInputEditText>(R.id.PasswordField)
+        val repeatPasswordField = view.findViewById<TextInputEditText>(R.id.RepeatPasswordField)
+        val registrationButton = view.findViewById<Button>(R.id.RegistrationButton)
+
+        passwordField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val shortPasswordFlag = view.findViewById<TextView>(R.id.ShortPassword)
+                if(passwordField.text?.length!! < 8){
+                    shortPasswordFlag.visibility = View.VISIBLE
+                } else {
+                    shortPasswordFlag.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+
+        repeatPasswordField.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val diffPasswordFlag = view.findViewById<TextView>(R.id.DiffPassword)
+                if(repeatPasswordField.text.toString() != passwordField.text.toString()){
+                    diffPasswordFlag.visibility = View.VISIBLE
+                } else {
+                    diffPasswordFlag.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
         val firstNameField = view.findViewById<TextInputEditText>(R.id.FirstNameField)
         val lastNameField = view.findViewById<TextInputEditText>(R.id.LastNameField)
         val patronymicField = view.findViewById<TextInputEditText>(R.id.PatronymicField)
         val numberField = view.findViewById<TextInputEditText>(R.id.PhoneField)
 
-        view.findViewById<Button>(R.id.RegistrationButton).setOnClickListener {
-            println("Onclick")
-            val requestBody = RegistrationRequest(
-                passwordField.text.toString(), numberField.text.toString(),
-                firstNameField.text.toString(), lastNameField.text.toString(),
-                patronymicField.text.toString(), emailField.text.toString()
-            )
-            val result = authorizationService.registerStudent(requestBody)
-                .enqueue(object : Callback<Profile> {
-                    override fun onFailure(call: Call<Profile>, t: Throwable) {
-                        System.out.println("result " + t.message)
-                    }
 
-                    override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
-                        if(response.code() == 200){
-                            activity?.startActivity(Intent(activity, RoomsActivity::class.java))
+
+        registrationButton.setOnClickListener {
+            val fillFieldsFlag = view.findViewById<TextView>(R.id.FillFields)
+//            if(emailField.text.isNullOrBlank() ||
+//                passwordField.text.isNullOrBlank()  ||
+//                passwordField.text != repeatPasswordField.text ||
+//                firstNameField.text.isNullOrBlank() ||
+//                lastNameField.text.isNullOrBlank() ||
+//                patronymicField.text.isNullOrBlank() ||
+//                numberField.text.isNullOrBlank() ){
+//                //fillFieldsFlag.visibility = View.VISIBLE
+//
+//            } else{
+                fillFieldsFlag.visibility = View.INVISIBLE
+                val requestBody = RegistrationRequest(
+                    passwordField.text.toString(), numberField.text.toString(),
+                    firstNameField.text.toString(), lastNameField.text.toString(),
+                    patronymicField.text.toString(), emailField.text.toString()
+                )
+                val result = authorizationService.registerStudent(requestBody)
+                    .enqueue(object : Callback<Profile> {
+                        override fun onFailure(call: Call<Profile>, t: Throwable) {
+                            System.out.println("result " + t.message)
                         }
-                        System.out.println("result " + response)
-                    }
-                })
+
+                        override fun onResponse(call: Call<Profile>, response: Response<Profile>) {
+                            if(response.code() == 200){
+                                activity?.startActivity(Intent(activity, RoomsActivity::class.java))
+                            }
+                            System.out.println("result " + response)
+                        }
+                    })
+//            }
+            println("Onclick")
+
         }
     }
 }
